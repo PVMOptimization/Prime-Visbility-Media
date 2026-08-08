b1ad98b10e0bd96a5f63131eac9d3336a5d70b12
PVMOptimization <optimization@primivisibilitymedia.com>
2026-08-08 23:42:42 +0000

    cnox

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -161,7 +161,7 @@ const FAQVideoSection = () => {
                 <iframe
                   width="100%"
                   height="100%"
-                  src="https://www.youtube.com/shorts/v9iNj1Fvaig"
+                  src="https://www.youtube.com/embed/v9iNj1Fvaig"
                   title="FAQ"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

618ef3fd1c08c6cb831aa641a9c5de52329ee92f
PVMOptimization <optimization@primivisibilitymedia.com>
2026-09-13 17:31:22 -0500

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/vNUrWpCVXJk"
+              src="https://www.youtube.com/embed/txX-mx9YMLU"
               title="System Demo"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
